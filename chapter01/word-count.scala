78e4cd7dda5b7334d13d0754f9fe23d2cb55b8ce
lemonskiller <yangzhiyugogogo@gmail.com>
2024-07-31 00:54:08 +0800

    -add: ph1

---
+++ chapter01/word-count.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.rdd.RDD
+
+// --- 读取内容 --- 
+// 这里的下划线"_"是占位符，代表数据文件的根目录
+val rootPath: String = "/Users/yangzhiyu/Documents/learn-spark/chapter01"
+val file: String = s"${rootPath}/wikiOfSpark.txt"
+ 
+// 读取文件内容
+val lineRDD: RDD[String] = spark.sparkContext.textFile(file) 
+
+// --- 分词 ---
+// 以行为单位做分词
+val wordRDD: RDD[String] = lineRDD.flatMap(line => line.split(" ")) 
+
+// 过滤掉空字符串
+val cleanWordRDD: RDD[String] = wordRDD.filter(word => !word.equals(""))
+
+// --- 分组计数 ---
+// 把RDD元素转换为（Key，Value）的形式
+val kvRDD: RDD[(String, Int)] = cleanWordRDD.map(word => (word, 1)) 
+
+// 按照单词做分组计数
+val wordCounts: RDD[(String, Int)] = kvRDD.reduceByKey((x, y) => x + y) 
+
+// 打印词频最高的5个词汇
+wordCounts.map{case (k, v) => (v, k)}.sortByKey(false).take(5)
